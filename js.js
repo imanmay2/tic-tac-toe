469b42b0ff24e8d5f57bdae33ddfe1e1715fa5fa
manmay2 <imanmay2@gmail.com>
2024-06-04 21:15:41 +0530

    added

--- js.js
+++ js.js
@@ -1,15 +1,21 @@
 let btn1=document.querySelector("#btn1");
-btn1.onclick=()=>{
+btn1.addEventListener("click",()=>{
     let user_1=prompt("Enter Player1's Name_ : ");
-    let user_2=prompt("Enter Player2's Name:_ : ");
+    
+    let user_2=prompt("Enter Player2's Name_: ");
 }
+)
 
 
 
 let btn2=document.querySelector("#btn2");
 btn2.addEventListener("click",()=>{
     let user_1=prompt("Enter Player's Name_ : ");
+    let med=prompt("Enter the medium of Game : Easy,Moderate or Tough ??");
 }
 )
+
+
+
 
 
