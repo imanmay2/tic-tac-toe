51f98462929d7c8374431c00646e6cd13c851f0a
manmay2 <imanmay2@gmail.com>
2024-06-04 20:49:54 +0530

    names taken via javascript

---
+++ js.js
@@ -0,0 +1,15 @@
+let btn1=document.querySelector("#btn1");
+btn1.onclick=()=>{
+    let user_1=prompt("Enter Player1's Name_ : ");
+    let user_2=prompt("Enter Player2's Name:_ : ");
+}
+
+
+
+let btn2=document.querySelector("#btn2");
+btn2.addEventListener("click",()=>{
+    let user_1=prompt("Enter Player's Name_ : ");
+}
+)
+
+
