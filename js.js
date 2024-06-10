5738a0a8b255923d14c34e6e693556f21e9299f4
manmay2 <imanmay2@gmail.com>
2024-06-10 08:04:08 +0530

    javascript portion yet to be deployed

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 let btn1=document.querySelector("#btn1");
 btn1.addEventListener("click",()=>{
     let user_1=prompt("Enter Player1's Name_ : ");
-    
     let user_2=prompt("Enter Player2's Name_: ");
 }
 )
