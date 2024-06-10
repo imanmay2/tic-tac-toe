9da3bc62f333d8ec96ee8df44dd12539de0abc46
manmay2 <imanmay2@gmail.com>
2024-06-10 20:10:05 +0530

    javascript code yet to be deployed

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+
+
+
 let btn1=document.querySelector("#btn1");
 btn1.addEventListener("click",()=>{
     let user_1=prompt("Enter Player1's Name_ : ");
@@ -13,6 +16,23 @@ btn2.addEventListener("click",()=>{
     let med=prompt("Enter the medium of Game : Easy,Moderate or Tough ??");
 }
 )
+
+
+
+
+let k=0;
+let btn3=document.querySelector(".box");
+btn3.addEventListener("click",()=>{
+    if(k==0){
+        btn3.innerText="X";
+        k=k+1;
+    }
+    else{
+        btn3.innerText="O";
+        k=k-1;
+    }
+});
+
 
 
 
